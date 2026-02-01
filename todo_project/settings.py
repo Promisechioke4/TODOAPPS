2a4c99efda9eaa6a192094e95d27e0705bd9dd48
Promisechioke4 <promzzygod90@gmail.com>
2026-02-01 18:07:11 +0100

    Ready for Render deployment

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+h5s(_d_#@-s^c^*pxyr3m5rak^fr@d69=^qd0+zu&!&!s5$v^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
